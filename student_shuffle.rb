9bdf87a0a9a8e2e8312f4d07ffea71aed7a92483
Cameron <cam@freshtape.com>
2015-01-29 09:38:46 -0500

    first commit

---
+++ student_shuffle.rb
@@ -0,0 +1,6 @@
+#Write a program that splits the class into teams of two
+#If there is an uneven number of students the last group should be a group of three
+
+students = ['Daniele', 'Alesia', 'Anita','Ben', 'Thomas', 'Matthew', 'Mark','Jennifer','Cameron']
+
+puts students
