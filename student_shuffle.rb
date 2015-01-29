4b1004ab569aa2180b4a8914937e87781d669ff8
Cameron <cam@freshtape.com>
2015-01-29 11:03:23 -0500

    update

--- student_shuffle.rb
+++ student_shuffle.rb
@@ -20,32 +20,37 @@ students.shuffle!
 count = 0
 num_students = students.length
 
+puts "\nYour teams are: "
+
 #until a count is the number of students
 until count == num_students
 	#odd students
-
-	if num_students % 2 == 1
-		
-		if count < num_students - 3
-		#outputs pairs of students
-			puts "#{students[count]} & #{students[count+1]}"
-		
+	if num_students==1
+		puts "#{students[0]} is a team of one"
+		break
 		else
-	
-			puts "#{students[count]},#{students[count+1]}, #{students[count+2]}"
-		  break
-		
+			if num_students % 2 == 1
+				
+				if count < num_students - 3
+				#outputs pairs of students
+					puts "#{students[count]} & #{students[count+1]}"
+				
+				else
+			
+					puts "#{students[count]},#{students[count+1]}, #{students[count+2]}"
+				  break
+				
+			end
+
+		  else #even # of students
+		  #output pairs of students
+
+			  puts "#{students[count]} & #{students[count+1]}"
+			#increase the count
 		end
-
-  else #even # of students
-  #output pairs of students
-
-	  puts "#{students[count]} & #{students[count+1]}"
-	#increase the count
-	
 	end
 	count += 2
 end #end until
 
 
-
+	
