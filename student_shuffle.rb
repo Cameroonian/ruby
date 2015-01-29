3ea336674ae91391fdfc41bdc6383b015bd6c049
Cameron <cam@freshtape.com>
2015-01-29 10:09:00 -0500

    update

--- student_shuffle.rb
+++ student_shuffle.rb
@@ -3,4 +3,22 @@
 
 students = ['Daniele', 'Alesia', 'Anita','Ben', 'Thomas', 'Matthew', 'Mark','Jennifer','Cameron']
 
-puts students
+students.shuffle!
+
+#set a count
+count = 0
+num_students = students.length - 1
+
+#until a count is the number of students
+until count == num_students
+
+#test loop
+puts "#{students[count]} & #{students[count+1]}"
+
+#increase the count
+count += 2
+
+end #end until
+
+
+
